b49a55e9d5d088cffd6c5b427430405884974b7a
Clemy-beep <clementine.digny@gmail.com>
2022-02-07 11:08:27 +0100

    vue tests

---
+++ src/views/Login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="login">
+    <h1>This is the login page.</h1>
+    <form action="" method="post" @submit="checkForm">
+        <!-- <p v-if="errors.lenght">
+            <b>Please correct the following errors :</b>
+            <ul>
+                <li v-for="error in errors" :key="error.id">{{error}}</li>
+            </ul>
+        </p> -->
+      <p>
+        <label for="movie">Favourite movie</label>
+        <select name="movie" id="movie" v-model="movie">
+          <option value="starwars">Star Wars</option>
+          <option value="fightclub">Fight Club</option>
+        </select>
+      </p>
+      <p>
+        <label for="firstname">First Name</label>
+        <input
+          type="text"
+          name="firstname"
+          id="firstname"
+          v-model="firstname"
+        />
+      </p>
+      <p>
+        <label for="lastname">Lastname</label>
+        <input type="text" name="lastname" id="lastname" v-model="lastname" />
+      </p>
+    </form>
+  </div>
+</template>
+
+<script>
+    
+</script>
